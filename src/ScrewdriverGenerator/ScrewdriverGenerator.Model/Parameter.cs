7bb9a39b8b6f47d9ab3e58653d9777518555bcff
baalp <baalp@list.ru>
2022-11-24 00:13:45 +0700

    feature(Parameter.cs): Добавление нового класса Parameter

---
+++ src/ScrewdriverGenerator/ScrewdriverGenerator.Model/Parameter.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScrewdriverGenerator.Model
+{
+    public class Parameter
+    {
+        private double _value;
+        private readonly double _minValue;
+        private readonly double _maxValue;
+        private readonly string _minErrorMessage;
+        private readonly string _maxErrorMessage;
+        private readonly ScrewdriverParameterType _screwdriverParameterType;
+        private readonly Dictionary<ScrewdriverParameterType, string> _errors;
+
+        public double Value
+        {
+            get => _value;
+            set
+            {
+                if (CheckRange(value))
+                {
+                    _value = value;
+                }
+            }
+        }
+
+        public Parameter
+            (
+            double value, 
+            double minValue, 
+            double maxValue,
+            string errorMessageAttachment,
+            ScrewdriverParameterType screwdriverParameterType,
+            Dictionary<ScrewdriverParameterType, string> errors
+            )
+        {
+            _errors = errors;
+            _minValue = minValue;
+            _maxValue = maxValue;
+            _minErrorMessage = 
+                "Error: " + errorMessageAttachment + " can't be less than " + _minValue + " mm.";
+            _maxErrorMessage = 
+                "Error: " + errorMessageAttachment + " can't be more than " + _maxValue + " mm.";
+            _screwdriverParameterType = screwdriverParameterType;
+            Value = value;
+        }
+
+        private bool CheckRange(double value)
+        {
+            if (value < _minValue)
+            {
+                _errors.Add(_screwdriverParameterType, _minErrorMessage);
+                return false;
+            }
+            else if (value > _maxValue)
+            {
+                _errors.Add(_screwdriverParameterType, _maxErrorMessage);
+                return false;
+            }
+            else return true;
+        }
+    }
+}
--- src/ScrewdriverGenerator/ScrewdriverGenerator.View/MainForm.cs
+++ src/ScrewdriverGenerator/ScrewdriverGenerator.View/MainForm.cs
@@ -15,6 +15,33 @@ namespace ScrewdriverGenerator.View
         public MainForm()
         {
             InitializeComponent();
+
+            // Проверка на ввод некорректных символов во все TextBox.
+            TextBoxTipRodHeight.KeyPress            += PreventInputWrongSymbols;
+            TextBoxWidestPartOfHandle.KeyPress      += PreventInputWrongSymbols;
+            TextBoxLengthOfOuterPartOfRod.KeyPress  += PreventInputWrongSymbols;
+            TextBoxLengthOfHandle.KeyPress          += PreventInputWrongSymbols;
+            TextBoxLengthOfInnerPartOfRod.KeyPress  += PreventInputWrongSymbols;
+        }
+
+        /// <summary>
+        /// Запрет ввода некорректных символов. Исключение - запятая на разделение дробной части.
+        /// </summary>
+        /// <param name="sender">TextBox - Инициатор события.</param>
+        /// <param name="e">Нажатая на клавиатуре клавиша.</param>
+        private static void PreventInputWrongSymbols(object sender, KeyPressEventArgs e)
+        {
+            if(
+                !char.IsControl(e.KeyChar) &&
+                !char.IsDigit(e.KeyChar) &&
+                !(
+                    e.KeyChar == ',' &&
+                    ((TextBox)sender).Text.IndexOf(",", StringComparison.Ordinal) == -1
+                )
+            )
+            {
+                e.Handled = true;
+            }
         }
     }
 }
